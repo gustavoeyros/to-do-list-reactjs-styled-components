ebc40eb005055f54453c6b9f2bf8252da0c5fe9b
Gustavo Eyros <gustavoeyros@hotmail.com>
2022-12-29 21:02:54 -0300

    feat: css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,7 @@
-import styled from "styled-components";
-
-import "./Button.css";
-
+import styles from "./Button.module.css";
+/* 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,6 +10,9 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  @media (min-width: 768px) {
+    width: auto;
+  }
   &:focus {
     outline: none;
   }
@@ -22,13 +24,14 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+ */
 
-/* const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} nClick={props.onClick}>
       {props.children}
     </button>
   );
-}; */
+};
 
 export default Button;
